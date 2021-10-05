c37eefdd70c7bc84702a8c0854df93b455a6ea9c
ИСП-31 <-31@LAB24-05>
2021-10-05 13:20:50 +0300

    Добавьте файлы проекта.

---
+++ Lib_5/Lib_5/Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Lib_5
+{
+    public class resh
+    {
+        /// <summary>
+        /// Вычисляет сумму целых случайных чисел, распределенных в диапазоне от Ot до Do,пока эта сумма не превышает некоторого числа SummDo
+        /// </summary>
+        /// <param name="Ot">число для рандома</param>
+        /// <param name="Do">число для рандома</param>
+        /// <param name="summDo">число которое не должно быть превышено</param>
+        /// <param name="generatMas">сгенерированые числа в строковой форме "1 2 3 4 ..."</param>
+        /// <param name="kolvoNum">количество сгенеррованых чисел</param>
+        /// <param name="summItog">сумма сгенерированых чисел</param>
+        public static void GeneretToSumm(int Ot, int Do, int summDo,out string generatMas,out int kolvoNum, out int summItog)
+        {
+            int a=0;
+            summItog = 0;
+            kolvoNum = 0;
+            generatMas = "";
+            Random rnd = new Random();
+            do
+            {
+                a = rnd.Next(Ot, Do);
+                if (a+ summItog <= summDo)
+                {
+                    summItog += a;
+                    kolvoNum++;
+                    generatMas += " " + a.ToString();
+                }
+            } while (a + summItog <= summDo);
+        }
+    }
+}
---
+++ var 5 pract 1/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Lib_5;
+
+namespace var_5_pract_1
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click_Generate(object sender, RoutedEventArgs e)
+        {
+            int k;
+            try
+            {
+                k = Convert.ToInt32(KTextBox.Text);
+                int kolvo;
+                int sumGeneret;
+                resh.GeneretToSumm(-1, 3, k, out string mas, out kolvo, out sumGeneret);
+                numbers.Text = mas;
+                SummTextBox.Text = sumGeneret.ToString();
+                KolvoTextBox.Text = kolvo.ToString();
+            }
+            catch { MessageBox.Show(" а ну заполни поле", "@приказ@"); }
+        }
+        private void Button_Click_exit(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+        private void Button_Click_info(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Косоуров И. Н.\nвариант 5 практическая 1\n\nВычислить сумму целых случайных чисел, распределенных в диапазоне от - 7 до 3,\nпока эта сумма не превышает некоторого числа K.Вывести на экран\nсгенерированные числа, значение суммы, и количество сгенерированных чисел.","информация о программе");
+        }
+        private void TextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            int val;
+            if (!Int32.TryParse(e.Text, out val) && e.Text != "-")
+            {
+                e.Handled = true; // отклоняем ввод
+            }
+        }
+    }   
+}
